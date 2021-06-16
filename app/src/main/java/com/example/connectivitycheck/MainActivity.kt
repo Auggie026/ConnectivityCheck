e570832960fc438cbf0272a655262ce5ed8b971a
Augusto Castro <augustocesarcastro026@gmail.com>
2021-06-16 00:47:42 -0300

    project concluded. This app checks if the internet is working or not. Launcher icon also  added

--- app/src/main/java/com/example/connectivitycheck/MainActivity.kt
+++ app/src/main/java/com/example/connectivitycheck/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.connectivitycheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.connectivitycheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var connectivityLiveData: ConnectionLiveData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        checkNetworkConnectionLiveData()
+    }
+
+    private fun checkNetworkConnectionLiveData(){
+
+        connectivityLiveData = ConnectionLiveData(application)
+
+        connectivityLiveData.observe(this, { isAvailable ->
+            when(isAvailable){
+                true -> binding.tvConnection.text = "Internet is working!"
+                false -> binding.tvConnection.text = "Internet is not working!"
+            }
+        })
     }
 }
